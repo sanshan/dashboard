f9e6fabdc462df445e134908f075a5c528dab7a0
Aleksandr Likhikh <sasha.lihih@gmail.com>
2020-04-01 13:00:01 +0300

    Added first amChart4

--- src/app/modules/dashboard/_mock/components/chart1/chart1.component.ts
+++ src/app/modules/dashboard/_mock/components/chart1/chart1.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {Component, ChangeDetectionStrategy, NgZone, AfterViewInit, OnDestroy} from '@angular/core';
+
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4charts from '@amcharts/amcharts4/charts';
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+
+am4core.useTheme(am4themes_animated);
 
 @Component({
   selector: 'app-chart1',
@@ -6,11 +12,57 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./chart1.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Chart1Component implements OnInit {
+export class Chart1Component implements AfterViewInit, OnDestroy {
 
-  constructor() { }
+  private chart: am4charts.XYChart;
 
-  ngOnInit(): void {
+  constructor(
+    private zone: NgZone
+  ) {
+  }
+
+  ngAfterViewInit() {
+    this.zone.runOutsideAngular(() => {
+      const chart = am4core.create('chartdiv', am4charts.XYChart);
+
+      // ... chart code goes here ...
+      chart.paddingRight = 20;
+      const data = [];
+      let visits = 10;
+      for (let i = 1; i < 366; i++) {
+        visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+        data.push({date: new Date(2018, 0, i), name: 'name' + i, value: visits});
+      }
+      chart.data = data;
+
+      const dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+      dateAxis.renderer.grid.template.location = 0;
+
+      const valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+      valueAxis.tooltip.disabled = true;
+      valueAxis.renderer.minWidth = 35;
+
+      const series = chart.series.push(new am4charts.LineSeries());
+      series.dataFields.dateX = 'date';
+      series.dataFields.valueY = 'value';
+
+      series.tooltipText = '{valueY.value}';
+      chart.cursor = new am4charts.XYCursor();
+
+      const scrollbarX = new am4charts.XYChartScrollbar();
+      scrollbarX.series.push(series);
+      chart.scrollbarX = scrollbarX;
+
+      this.chart = chart;
+    });
+  }
+
+  ngOnDestroy() {
+    this.zone.runOutsideAngular(() => {
+      if (this.chart) {
+        this.chart.dispose();
+      }
+    });
   }
 
 }
