1d69e95c7acf5935388fffbe0c23bbe5e83f1ced
Aleksandr Likhikh <sasha.lihih@gmail.com>
2020-04-06 12:59:58 +0300

    Added back button && fix bug with empty groups display

--- src/app/modules/dashboard/components/dashboard-grid-settings/dashboard-grid-settings.component.ts
+++ src/app/modules/dashboard/components/dashboard-grid-settings/dashboard-grid-settings.component.ts
@@ -10,97 +10,91 @@ import {DashboardGridSettingsFetchService} from '../../services/dashboard-grid/s
 
 
 @Component({
-  selector: 'app-dashboard-grid-settings',
-  templateUrl: './dashboard-grid-settings.component.html',
-  styleUrls: ['./dashboard-grid-settings.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-dashboard-grid-settings',
+    templateUrl: './dashboard-grid-settings.component.html',
+    styleUrls: ['./dashboard-grid-settings.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardGridSettingsComponent implements OnInit, AfterViewInit {
-  constructor(
-    private _dgs: DashboardGridSettingsService,
-    private _dgo: DashboardGridOptionsService,
-    private _dgf: DashboardGridSettingsFormService,
-    private _dgfch: DashboardGridSettingsFetchService,
-    private _cdr: ChangeDetectorRef,
-    private _u: UserService
-  ) {
-  }
+    constructor(
+        private _dgs: DashboardGridSettingsService,
+        private _dgo: DashboardGridOptionsService,
+        private _dgf: DashboardGridSettingsFormService,
+        private _dgfch: DashboardGridSettingsFetchService,
+        private _cdr: ChangeDetectorRef,
+        private _u: UserService
+    ) {
+    }
 
-  get user() {
-    return this._u.user;
-  }
+    get user() {
+        return this._u.user;
+    }
 
-  get params$() {
-    return this._dgs.params$;
-  }
+    get params$() {
+        return this._dgs.params$;
+    }
 
-  get form(): FormGroup {
-    return this._dgs.form;
-  }
+    get form(): FormGroup {
+        return this._dgs.form;
+    }
 
-  ngOnInit(): void {
-    /** Сделим за изменениями в форме */
-    this._settingsFormSubscribe();
+    ngOnInit(): void {
+        /** Сделим за изменениями в форме */
+        this._settingsFormSubscribe();
 
-    this._userSubscribe();
-  }
+        this._userSubscribe();
+    }
 
-  ngAfterViewInit(): void {
-    this._dgo.updateOptionSubject(
-      this._dgo.settingsToOptions(
-        this._dgf.formValue
-      )
-    );
-  }
+    ngAfterViewInit(): void {
+        this._dgo.updateOptionSubject(
+            this._dgo.settingsToOptions(
+                this._dgf.formValue
+            )
+        );
+    }
 
-  /** Получить группу контролов из реактивной формы */
-  getGroup(value): FormGroup {
-    return this.form.get(value) as FormGroup;
-  }
+    /** Получить группу контролов из реактивной формы */
+    getGroup(value): FormGroup {
+        return this.form.get(value) as FormGroup;
+    }
 
-  /** Подписываемся и следим за изменениями в форме */
-  private _settingsFormSubscribe() {
-    this.form.valueChanges.subscribe(this.sendOptionsToGrid);
-  }
+    /** Подписываемся и следим за изменениями в форме */
+    private _settingsFormSubscribe() {
+        this.form.valueChanges.subscribe(this.sendOptionsToGrid);
+    }
 
-  /** Подписываемся и следим за измененим пользователя */
-  private _userSubscribe() {
-    this._u.user$.subscribe((user) => {
-      console.log('UPDATE: ', user);
-      this._cdr.markForCheck();
-    });
-  }
+    /** Подписываемся и следим за измененим пользователя */
+    private _userSubscribe() {
+        this._u.user$.subscribe((user) => {
+            console.log('UPDATE: ', user);
+            this._cdr.markForCheck();
+        });
+    }
 
+    /** Отправка опций в сервис грида */
+    sendOptionsToGrid = (groups: FormGroup) => {
+        this._dgo.updateOptionSubject(this._dgo.settingsToOptions(groups));
+    }
 
-  /** Отправка опций в сервис грида */
-  sendOptionsToGrid = (groups: FormGroup) => {
-    this._dgo.updateOptionSubject(this._dgo.settingsToOptions(groups));
-  }
+    switchUser(role: Role) {
+        const groups = this._dgfch.getSettingsData()
+            .map((group) => {
+                return {
+                    title: group.title,
+                    controls: group.controls.filter((control) => {
+                        return control.access.indexOf(role) !== -1;
+                    })
+                } as GridParamGroupInterface;
+            });
+        this._dgfch.updateSettingsSubject(groups);
+    }
 
-  switchUser(role: Role) {
-    console.log('ACTIVATE ROLE: ', role);
+    back() {
+        window.history.back();
+    }
 
-    console.log('BEFORE switchUser(): ', this._dgfch.settingsValue);
-
-    const groups = this._dgfch.getSettingsData()
-      .map((group) => {
-        return {
-          title: group.title,
-          controls: group.controls.filter((control) => {
-            return control.access.indexOf(role) !== -1;
-          })
-        } as GridParamGroupInterface;
-      });
-
-    console.log('AFTER switchUser(): ', groups);
-
-    this._dgfch.updateSettingsSubject(groups);
-
-    // console.log(groups);
-
-    //
-    // this._u.updateUserSubject(role);
-    // this._cdr.markForCheck();
-  }
+    controlsAvailable(group: GridParamGroupInterface) {
+        return group.controls.length > 0;
+    }
 
 }
